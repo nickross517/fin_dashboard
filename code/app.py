e329f8c7eae16c6578776207ef42760d2223fe7e
Nick Ross <nick@Nicks-MBP.lan>
2023-01-25 11:15:42 -0500

    added docker stuff and tweaked db setup

--- code/app.py
+++ code/app.py
@@ -1,16 +1,78 @@
+# for getting the data from Yahoo finance
+import requests
+import json
 import pandas as pd
 from sqlalchemy import create_engine
+# for plots
 import dateutil.relativedelta as rdelta
 import plotly.express as px
 from dash import Dash, html, dcc
 import plotly.graph_objects as go
 
-from get_data import postgres_user, postgres_password, database
+######### change these to env vars or whatever is best practice
+postgres_user='root'
+postgres_password='root'
+database='database'
+port='5432'
+# host is the name of your postgres container
+host='postgres_db'
 
+params = {
+    'interval': '1d',
+    'range': 'ytd',
+}
 
-eng=create_engine(f"postgresql://{postgres_user}:{postgres_password}@localhost/{database}")
+etf_list=['xle','xlf','xlu','xli','xlk','xlv','xly','xlp','xlb']
+
+headers = {
+    'authority': 'query1.finance.yahoo.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'accept-language': 'en-US,en;q=0.5',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Brave";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'sec-gpc': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+}
+
+data=pd.DataFrame()
+for ticker in etf_list:
+    r=requests.get(f'https://query1.finance.yahoo.com/v8/finance/chart/{ticker}', params=params, headers=headers)
+    response=json.loads(r.text)
+    prices=pd.json_normalize(response['chart']['result'][0]['indicators']['quote'][0]).explode(['high','low','close','open','volume']).reset_index(drop=True)
+    prices['ticker']=ticker
+    # TODO: dont parse this more than once 
+    timestamps=response['chart']['result'][0]['timestamp']
+    ticker_data=pd.concat([pd.Series(timestamps, name='date'), prices], axis=1)
+    data=pd.concat([data, ticker_data]).reset_index(drop=True)
+data['date']=pd.to_datetime(data['date'],unit='s')
+data=data.sort_values(by=['date','ticker']).reset_index(drop=True)
+
+# loading to postgres
+eng = create_engine(f"postgresql://{postgres_user}:{postgres_password}@{host}:{port}/{database}")
+data.to_sql(name="dashboard", con=eng, if_exists='append', index=False)
+
+remove_dupes_query="""
+delete from dashboard where id in 
+ (select id from 
+    (select t.*, row_number() over (partition by to_char(date,'YYYY-MM-DD'), ticker) as seqnum from dashboard t 
+    order by ticker, date desc) as t
+     where seqnum>1);
+"""
+#eng.execute(remove_dupes_query)
+print('finished loading data into postgres')
+
+### Reformatting data and plotting
+eng=create_engine(f"postgresql://{postgres_user}:{postgres_password}@{host}:{port}/{database}")
 pg_data=pd.read_sql("select * from dashboard", con=eng)
 
+# TODO: - put these into a csv/load into postgres as dim table - "preferred names"
 ticker_names=pd.DataFrame.from_dict({'ticker':['xlb','xle','xlf','xli','xlk','xlp','xlu','xlv','xly'],\
  'full_names':['Materials (XLB)','Energy (XLE)','Financials (XLF)','Industrials (XLI)','Technology (XLK)',\
     'Consumer Staples (XLP)','Utilities (XLU)','Health Care (XLV)','Consumer Discretionary (XLY)']})
@@ -24,9 +86,8 @@ def weekly_returns(df):
     weekly_gb['returns'] = (weekly_gb['last'] / weekly_gb['first']) -1
     
     # Handles Mondays or holidays at beginning of the week for weekly returns chart
-    # Checks if day is Monday or if all returns are zero for the week (possible that all returns can actually be zero but unlikely enough to leave it for now)
+    # Checks if day is Monday or if all returns are zero for the week (possible that all returns can actually be zero but unlikely enough to leave it)
     if (pd.to_datetime('today').dayofweek == 0) | (all(0 == x for x in list(weekly_gb.iloc[0:9,:]['returns']))):
-        #### Add something to check if all returns are zero for given week (eg if monday was holiday than tuesday will zero out)
         weekly_gb=weekly_gb.loc[(weekly_gb['month']==(pd.to_datetime('today').month)) & (weekly_gb['week']==(pd.to_datetime('today').week -1 ))]
         last_monday=(pd.to_datetime('today') + rdelta.relativedelta(days=-1, weekday=rdelta.MO(-1))).strftime('%m/%d')
         last_friday=(pd.to_datetime('today') + rdelta.relativedelta(days=-1, weekday=rdelta.FR(-1))).strftime('%m/%d')
@@ -108,5 +169,5 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
 
